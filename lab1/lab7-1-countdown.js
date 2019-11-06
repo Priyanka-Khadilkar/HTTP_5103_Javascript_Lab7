fc36e576475bdd7d17b9dd0e741d2c9c51a33dc1
Priyanka Khadilkar <49756361+Priyanka-Khadilkar@users.noreply.github.com>
2019-11-06 10:56:31 -0500

    Add files via upload

---
+++ lab1/lab7-1-countdown.js
@@ -0,0 +1,42 @@
+﻿/* LAB 7-1 - FINAL COUNTDOWN!! */
+//Please delete once you have confirmed your page is connected...seriously, if I see this popup when I check out your work, I will go coo-coo bananas.
+
+//create page load listener
+window.onload = pageready;
+
+//create page load function
+function pageready(){
+	
+	//create variables for required HTML elements
+	var todayData = document.getElementById("todayData");
+	var finalData = document.getElementById("finalData");
+	var dueData =  document.getElementById("dueData");
+	var timelabel = document.getElementById("timelabel");
+	var countMsg =  document.getElementById("countMsg");
+	//create variables for now date and due date
+	var dateVar = new Date();
+	var nowDateTime = dateVar.getTime();
+	
+	var dueDate = new Date("December 11, 2020 23:59:00");
+	
+	var compareDate = new Date("December 11, 2019 23:59:00");
+	compareDate = new Date(compareDate.getTime());
+	
+	//CONVERT TO UTC AND SUBTRACT TO GET TIME DIFFERENCE
+	dueDate = new Date(dueDate.getTime());
+	var timeDiff = dueDate - nowDateTime;
+	
+	//CONVERT TIME DIFFERENCE TO WHOLE NUMBER OF DAYS
+	var daysTillDueDate = timeDiff/86400000;
+    var fullDays = Math.floor(daysTillDueDate) ;
+	//OUTPUT NOW DATE & DUE DATE TO HTML PAGE
+	todayData.innerHTML = dateVar.toDateString();
+	finalData.innerHTML = dueDate .toDateString();
+	dueData.innerHTML = fullDays;
+	//LOGIC TO CHECK IF DUE DATE HAS PASSED, AND OUPUT APPROPRIATE MESSAGE TO HTML PAGE
+	if(dueDate > compareDate)
+	{
+		countMsg.innerHTML = "the deadline for the final Assignment has passed";
+	}
+	
+}
---
+++ lab2/lab7-2-clock.js
@@ -0,0 +1,51 @@
+﻿/* LAB 7.2 - STOP TIME */
+
+//create page load listener
+window.onload = pageready;
+//create page load function
+function pageready(){
+	//create variables for required HTML elements
+	var hoursOut = document.getElementById("hoursOut");
+	var minsOut = document.getElementById("minsOut");
+	var secsOut =  document.getElementById("secsOut");
+	var btnStart = document.getElementById("btnStart");
+	var btnStop = document.getElementById("btnStop");
+
+	//create time variable so all functions have access to it
+	var tickTock;
+	var dateVar,hours,mins,secs;
+	
+	//CREATE FUNCTION THAT DISPLAYS THE TIME
+	function displayTime()
+	{
+		dateVar = new Date();
+		hours = dateVar.getHours();
+		hoursOut.innerHTML = hours + ":";
+		
+		mins = dateVar.getMinutes();
+		minsOut.innerHTML = mins+ ":";
+		
+		secs = dateVar.getSeconds();
+		secsOut.innerHTML = ("0" + parseInt(secs)).slice(-2);
+        		
+	}
+	//CREATE FUNCTION TO START THE CLOCK.
+	function start()
+	{		
+		displayTime();
+		tickTock = setInterval(displayTime,1000);
+		
+	}
+	
+	//CREATE FUNCTION TO STOP THE CLOCK
+	function stop(){
+		
+		clearInterval(tickTock);
+	}
+	
+	// SET EVENT LISTENERS
+	btnStart.onclick = start;
+	btnStop.onclick = stop;
+	
+	
+}
